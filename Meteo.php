5f32a0b17e5e31fe8ec33f2ee2b2f2999d2017c7
gianpaolo <gianpaolonettis@alice.it>
2019-06-25 16:56:26 +0200

    Create Meteo.php

---
+++ Meteo.php
@@ -0,0 +1,239 @@
+<?php
+
+function queryWeather($city){
+
+$ch = curl_init();
+$json_data = null;
+//$link = "api.openweathermap.org/data/2.5/forecast/hourly?q=Bari,IT&units=metric&APPID=dc994ecccf460974d34e32cde11ce679";
+$link2 = "api.openweathermap.org/data/2.5/forecast?q=".$city.",IT&units=metric&APPID=dc994ecccf460974d34e32cde11ce679";
+curl_setopt($ch, CURLOPT_URL,$link2);
+curl_setopt($ch, CURLOPT_POST, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec($ch);
+$json_data = json_decode($server_output,true);
+curl_close ($ch);
+
+return $json_data;
+
+}
+
+function getTodayWeather($city,$parameters,$text){
+
+$json_data = queryWeather($city);
+$today = date('Y-m-d');
+$result ="" ;
+foreach ($json_data['list'] as $key => $value) {
+    $data = substr($value['dt_txt'], 0,10);
+    $hour = substr($value['dt_txt'], 11,2);
+    $temp = 0;
+    $description = "";
+     if($data == $today){
+        $temp = $value['main']['temp'];
+            
+          foreach ($value['weather'] as $key2 => $value2) {
+           $val = 	$value2['main'];
+           $description = $value2['description'];
+          }
+          switch ($description) {
+          	case 'clear sky':
+          		$condition = "cielo sereno";
+          		break;
+
+          	case 'few clouds':
+          		$condition = "poco nuvoloso";
+          		break;
+
+          	case 'broken clouds':
+          		$condition = "parzialmente nuvoloso";
+          		break;
+
+          	case 'scattered clouds':
+          		$condition = "nubi sparse";
+          		break;
+
+          	case 'moderate rain':
+          		$condition = "piogge modeste";
+          		break;
+
+          	case 'overcast clouds':
+          		$condition = "nuvoloso";
+          		break;
+
+          	case 'light rain':
+          		$condition = "pioggia leggera";
+          		break;
+
+          	case 'heavy rain':
+          		$condition = "pioggia pesante";
+          		break;
+          	
+          	default:
+          		# code...
+          		break;
+          }
+       $result.= $data.";ore:".$hour.";temperatura ".$temp."°;".$condition;
+     }
+   
+	$result .= "<br>";
+}
+
+return $result;
+
+}
+
+
+function getWeather($city,$parameters,$text){
+
+if(isset($parameters['date'])){
+$date = substr($parameters['date'],0,10);
+}else{
+$date = date('Y-m-d',strtotime("+1days")); 
+}
+
+$json_data = queryWeather($city);
+
+$result ="" ;
+foreach ($json_data['list'] as $key => $value) {
+    $data = substr($value['dt_txt'], 0,10);
+    $hour = substr($value['dt_txt'], 11,2);
+    $temp = 0;
+    $description = "";
+     if($data == $date){
+        $temp = $value['main']['temp'];
+            
+          foreach ($value['weather'] as $key2 => $value2) {
+           $val = 	$value2['main'];
+           $description = $value2['description'];
+          }
+          switch ($description) {
+          	case 'clear sky':
+          		$condition = "cielo sereno";
+          		break;
+
+          	case 'few clouds':
+          		$condition = "poco nuvoloso";
+          		break;
+
+          	case 'broken clouds':
+          		$condition = "parzialmente nuvoloso";
+          		break;
+
+          	case 'scattered clouds':
+          		$condition = "nubi sparse";
+          		break;
+
+          	case 'moderate rain':
+          		$condition = "piogge modeste";
+          		break;
+
+          	case 'overcast clouds':
+          		$condition = "nuvoloso";
+          		break;
+
+          	case 'light rain':
+          		$condition = "pioggia leggera";
+          		break;
+
+          	case 'heavy rain':
+          		$condition = "pioggia pesante";
+          		break;
+          	
+          	default:
+          		# code...
+          		break;
+          }
+       $result.= $data.";ore:".$hour.";temperatura ".$temp."°;".$condition."<br>";
+     }
+   
+	
+}
+
+$result.="<br><br><br>";
+return $result;
+
+
+}
+
+
+function binaryWeather($city,$parameters,$text){
+
+
+if(isset($parameters['date'])){
+$date = substr($parameters['date'],0,10);
+}else{
+$date = date('Y-m-d'); 
+}
+
+$json_data = queryWeather($city);
+$answer ="" ;
+$arr = array();
+
+foreach ($json_data['list'] as $key => $value) {
+    $data = substr($value['dt_txt'], 0,10);
+    $hour = substr($value['dt_txt'], 11,2);
+    $temp = 0;
+    $description = "";
+     if($data == $date && $hour >= 6){
+        $temp = $value['main']['temp'];
+            
+          foreach ($value['weather'] as $key2 => $value2) {
+           $val = 	$value2['main'];
+           array_push($arr, $val);
+           $description = $value2['description'];
+          }
+
+     }
+   }
+
+
+      $result= array_count_values ($arr);
+    
+   if (strpos($text, 'piove') !== false || strpos($text,'pessimo') !== false) {
+      
+   	if(isset($result['Rain']) && $result['Rain'] > 0){
+   		$answer = "Si,pioverà";
+   	}else{
+   		$answer = "No,non pioverà";
+   	}
+
+   }elseif (strpos($text,'brutto') !== false || strpos($text, 'cattivo') !== false) {
+   	
+   	if(isset($result['Rain']) && $result['Rain'] > 0){
+   		$answer = "Si,sarà piovoso";
+   	}elseif(isset($result['Cloud']) && $result['Cloud'] >= 3 ){
+   		$answer = "Si sarà nuvoloso";
+   	}else {
+   		$answer = "No,sarà sereno";
+   	}
+
+   }elseif (strpos($text, 'nuvoloso') !== false) {
+ 
+      if(isset($result['Cloud']) && $result['Cloud'] >= 3 ){
+   		$answer = "Si sarà nuvoloso";
+   	}elseif(isset($result['Rain']) && $result['Rain'] > 0) {
+   		$answer = "No,sarà piovoso";
+   	}else{
+   		$answer = "no,sarà sereno";
+   	}
+
+   	
+   }elseif(strpos($text, 'sole') !== false || strpos($text, 'buono') !== false || strpos($text, 'bello') !== false){
+
+   	 if(isset($result['Cloud']) && $result['Cloud'] >= 3 ){
+   		$answer = "No sarà nuvoloso";
+   	}elseif(isset($result['Rain']) && $result['Rain'] > 0) {
+   		$answer = "No,sarà piovoso";
+   	}else{
+   		$answer = "Si,sarà sereno";
+   	}
+
+   }
+	
+return $answer;
+
+
+
+}
+
+?>
